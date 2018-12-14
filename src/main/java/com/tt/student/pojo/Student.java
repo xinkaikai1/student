00827715954b510d7fc75c160850e61127a20588
xinkaikai1 <18636740768@163.com>
2018-12-14 20:42:09 +0800

    ddd

--- src/main/java/com/tt/student/pojo/Student.java
+++ src/main/java/com/tt/student/pojo/Student.java
@@ -1,7 +1,13 @@
 package com.tt.student.pojo;
 
 public class Student {
+    private String name;
 
-    
-    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
