50acd323f2a61965d8d5d99df916fcff23047d56
xinkaikai1 <18636740768@163.com>
2018-12-14 20:43:08 +0800

    vv

--- src/main/java/com/tt/student/pojo/Student.java
+++ src/main/java/com/tt/student/pojo/Student.java
@@ -2,12 +2,5 @@ package com.tt.student.pojo;
 
 public class Student {
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    
 }
